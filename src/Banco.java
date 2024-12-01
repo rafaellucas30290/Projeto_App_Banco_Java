34bc7b4704e1e48a38b1254be21359d66e78cc1f
Fernandinho21 <fnando0008@gmail.com>
2024-12-01 17:57:31 -0300

    Class banco

---
+++ src/Banco.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+import Model.*;
+import Interface.*;
+
+public class Banco implements BancoOperacoes {
+    private List<Conta> contas;
+
+    public Banco() {            
+        this.contas = new ArrayList<>();
+    }
+
+    public void adicionarConta(Conta conta) {
+        contas.add(conta);
+    }
+
+    @Override
+    public void depositar(Conta conta, double valor) {
+        conta.setSaldo(conta.getSaldo() + valor);
+    }
+
+    @Override
+    public void sacar(Conta conta, double valor) {
+        if (conta.getSaldo() >= valor) {
+            conta.setSaldo(conta.getSaldo() - valor);
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+
+    @Override
+    public void transferir(Conta origem, Conta destino, double valor) {
+        if (origem.getSaldo() >= valor) {
+            sacar(origem, valor);
+            depositar(destino, valor);
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+}
